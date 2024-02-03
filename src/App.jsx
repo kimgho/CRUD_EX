91cfee2cb72548082db6ef9bb7fbf138e1135f44
kimgho <kgh67811@naver.com>
2024-02-03 22:25:21 +0900

    BrowserRouter -> HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./Register";
 import Find from "./Find";
 import Home from "./Home";
